13000aaccaef0e01996dfbca6f59268f5ebf2612
DanCr <dancrkyt@gmail.com>
2022-04-01 14:49:22 -0600

    Data class para recibir los datos de la API

---
+++ app/src/main/java/com/dann/moviesapp/data/model/Movie.kt
@@ -0,0 +1,17 @@
+package com.dann.moviesapp.data.model
+
+data class Movie(
+    val adult: Boolean = false,
+    val backdrop_path: String = "_",
+    val id: Int = 0,
+    val original_language: String = "_",
+    val original_title: String = "_",
+    val overview: String = "_",
+    val popularity: Double = 0.0,
+    val poster_path: String = "_",
+    val release_date: String = "_",
+    val title: String = "_",
+    val video: Boolean = false,
+    val vote_average: Double = 0.0,
+    val vote_count: Int = 0
+)
