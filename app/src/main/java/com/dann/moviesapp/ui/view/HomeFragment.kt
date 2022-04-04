25280628d7bab5d35808969156f17bfeff96e7b1
DanCr <dancrkyt@gmail.com>
2022-04-04 17:38:13 -0500

    Prgoressbar para darle feedback al usuario

--- app/src/main/java/com/dann/moviesapp/ui/view/HomeFragment.kt
+++ app/src/main/java/com/dann/moviesapp/ui/view/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
@@ -41,6 +42,10 @@ class HomeFragment : Fragment() {
 
         vm.movies.observe(viewLifecycleOwner){ movies ->
             setReciclerView(movies)
+        }
+
+        vm.isLoading.observe(viewLifecycleOwner){ isloading ->
+            binding.progressBar.isVisible = isloading
         }
     }
 
--- app/src/main/java/com/dann/moviesapp/ui/view/PlayingNowFragment.kt
+++ app/src/main/java/com/dann/moviesapp/ui/view/PlayingNowFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
@@ -40,6 +41,10 @@ class PlayingNowFragment : Fragment() {
 
         vm.movies.observe(viewLifecycleOwner){ movies ->
             setReciclerView(movies)
+        }
+
+        vm.isLoading.observe(viewLifecycleOwner){ isloading ->
+            binding.progressBar.isVisible = isloading
         }
     }
 
